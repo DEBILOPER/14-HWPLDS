051f7c7b822e751e39da51f5ff9023a96672e871
Engr. Max Angelo Perin <perinmaks@outlook.ph>
2019-12-19 18:34:56 +0800

    Itlog
    Suwayi sa daw ni itlog

---
+++ HWPLDS-2/HWPLDS-2.ino
@@ -0,0 +1,12 @@
+#include "FLOWRATE1.h"
+Flowrate_1 f;
+
+void setup() {
+  // put your setup code here, to run once:
+f.setup1();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ f.flr1_loop()();
+}
