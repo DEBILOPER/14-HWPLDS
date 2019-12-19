859521df97c454ba9452d233f2b44fb918038f57
GeeandZee <35626927+GeeandZee@users.noreply.github.com>
2019-12-19 11:33:19 +0800

    Revert "Revert "Create bluetooth.ino""
    This reverts commit 96c5b23629be3bf90bd9a431a97e7f3610e164dc.

---
+++ bluetooth.ino
@@ -0,0 +1,22 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(8,OUTPUT);
+
+}
+
+void loop() {
+  if(Serial.available()>0)
+  {
+    char data = Serial.read();
+    if (data == 'a')
+    {
+      digitalWrite(8,HIGH);
+    }
+    else if(data == 'b')
+    {
+      digitalWrite(8,LOW);
+    }
+  }
+
+
+}
