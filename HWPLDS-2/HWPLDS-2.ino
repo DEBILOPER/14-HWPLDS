1ab71e6c41a689c7dd570e87c006d5ae9f8dd0d1
Engr. Max Angelo Perin <perinmaks@outlook.ph>
2019-12-20 13:53:57 +0800

    Update HWPLDS-2.ino

--- HWPLDS-2/HWPLDS-2.ino
+++ HWPLDS-2/HWPLDS-2.ino
@@ -12,13 +12,14 @@ ff.setup2();
 void loop() {
     fr2 = ff.flr2_loop();
     fr1 = f.flr1_loop();
-    
+    Serial.print("Flowrate 1: ");
     Serial.print(fr2);   // Print the integer part of the variable
     Serial.print("L/min "); 
     Serial.print(" | ");            // Print tab space
     delay (750);
+    Serial.print("Flowrate 2: ");
     Serial.print(fr1);    // Print the integer part of the variable
-    Serial.print("L/min "); 
+    Serial.println("L/min "); 
     delay (500);
 
 }
